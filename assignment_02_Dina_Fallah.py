a89b9ba138aed0516dc706028e839a3f9a3e0638
Dina Fallah <dina.a.fallah@gmail.com>
2023-11-04 15:55:37 +0200

    Fixed a few things with the second assignment

--- assignment_02_Dina_Fallah.py
+++ assignment_02_Dina_Fallah.py
@@ -1,10 +1,10 @@
-def digit_count_recursive(number):
+def degitCountRec(number):
     if number == 0:
         return 0
     else:
-        return 1 + digit_count_recursive(number // 10)
+        return 1 + degitCountRec(number // 10)
     
-def max_lookup(our_list):
+def maxLookup(our_list):
     if not our_list:
         return None
 
@@ -14,11 +14,11 @@ def max_lookup(our_list):
     first_element = our_list[0]
     rest_of_list = our_list[1:]
 
-    max_in_rest = max_lookup(rest_of_list)
+    max_in_rest = maxLookup(rest_of_list)
 
     return max_in_rest if max_in_rest > first_element else first_element  
   
-def input_list():
+def inputList():
     while True:
         input_str = input("Enter a list of numbers separated by spaces: ")
         input_list = input_str.split()
@@ -30,14 +30,14 @@ def input_list():
             print("Invalid input. Please stop wasting my time.")
  
  #Last ex           
-def is_2d_matrix(matrix):
+def isMatrix(matrix):
     if not all(isinstance(row, list) for row in matrix):
         return False 
     if len(set(len(row) for row in matrix)) != 1:
         return False 
     return True
 
-def input_2d_matrix():
+def inputMatrix():
     matrix = []
     
     while True:
@@ -57,7 +57,7 @@ def input_2d_matrix():
                     break
                 matrix.append([int(x) for x in row])
             
-            if is_2d_matrix(matrix):
+            if isMatrix(matrix):
                 return matrix
             else:
                 print("Invalid input. Please make sure all rows have the same number of elements.")
@@ -65,29 +65,29 @@ def input_2d_matrix():
         except ValueError:
             print("Invalid input. Please enter valid integers for rows and columns.")
 
-def our_mean(column):
+def ourMean(column):
     return sum(column) / len(column)
 
-def standard_deviation(column, mean):
+def standardDeviation(column, mean):
     squared_diff = [(x - mean) ** 2 for x in column]
     variance = sum(squared_diff) / len(column)
     std_deviation = variance ** 0.5
     return std_deviation
 
-def matrix_final_check(matrix, column = 0, count = 0):
+def matrixFinalCheck(matrix, column = 0, count = 0):
     num_columns = len(matrix[0])
 
     if column == num_columns:
         return count
 
     current_column = [row[column] for row in matrix]
-    mean = our_mean(current_column)
-    std_deviation = standard_deviation(current_column, mean)
+    mean = ourMean(current_column)
+    std_deviation = standardDeviation(current_column, mean)
 
     if mean == 0 and std_deviation == 1:
         count += 1
 
-    return matrix_final_check(matrix, column + 1, count)
+    return matrixFinalCheck(matrix, column + 1, count)
 
 def main():
     while True:
@@ -100,14 +100,14 @@ def main():
         
         if choice == 1:
             firstEx = int(input("Please give me a number: "))  # Code goes boom if you don't give a number
-            print(digit_count_recursive(firstEx))
+            print(degitCountRec(firstEx))
         elif choice == 2:
-            our_list = input_list()
-            max_value = max_lookup(our_list)
+            our_list = inputList()
+            max_value = maxLookup(our_list)
             print("The maximum value in the list is:", max_value)
         elif choice == 3:
-            matrix = input_2d_matrix()
-            count = matrix_final_check(matrix) 
+            matrix = inputMatrix()
+            count = matrixFinalCheck(matrix) 
             if count > 0:
                 print(f"{count} column(s) have a mean of 0 and a standard deviation of 1.")
             else:
