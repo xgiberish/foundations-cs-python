8b5a2bcce1a783cfac39da9f07560a6de420ad35
Dina Fallah <dina.a.fallah@gmail.com>
2023-11-12 11:25:23 +0200

    Initial setup

---
+++ midterm_Dina_fallah.py
@@ -0,0 +1,66 @@
+def openTab():
+    return
+
+def closeTab():
+    return
+
+def switchTab():
+    return
+
+def displayAllTabs():
+    return
+
+def openNestedTabs():
+    return
+
+def clearAllTabs():
+    return
+
+def saveTabs():
+    return
+
+def importTabs():
+    return
+
+
+
+
+def main():
+    while True:
+        print("\nOptions:")
+        print("1. Open Tab")
+        print("2. Close Tab")
+        print("3. Switch tab")
+        print("4. Display All tabs")
+        print("5. Open Nested tab")
+        print("6. Clear All Tabs")
+        print("7. Save Tabs")
+        print("8. Import Tabs")
+        print("9. Exit")
+
+        choice = input("What would you like to do?: ")
+
+        if choice == "1":
+            print()
+        elif choice == "2":
+            print()
+        elif choice == "3":
+            print()
+        elif choice == "4":
+            print()
+        elif choice == "5":
+            print()
+        elif choice == "6":
+            print()
+        elif choice == "7":
+            print()
+        elif choice == "8":
+            print()
+        elif choice == "9":
+            print("Goodbye.")
+            break
+        else:
+            print("Invalid choice. Please choose a valid operation (1-9).")
+
+
+main()
